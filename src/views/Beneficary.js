a96a9d6ee4bfcd346f81fdbe3568ec6b4cf85e42
Naqash ali <naqash.jutt1@gmail.com>
2023-03-29 19:53:23 +0500

    accident form

--- src/views/Beneficary.js
+++ src/views/Beneficary.js
@@ -241,6 +241,84 @@ function Beneficary() {
                 </Form>
               </Card.Body>
             </Card>
+
+
+            <Card>
+              <Card.Header>
+                <Card.Title as="h4">Accident</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Form>
+                  <Row>
+                    
+                    <Col className="px-1" md="4">
+                    <Form.Group>
+                        <label class="form-check-label">Accident Type</label>
+                        <Col className="pr-1" md="12">
+                          <select class="custom-select" id="gender2">
+                            <option selected>Choose...</option>
+                            <option value="1">Institution</option>
+                            <option value="2">Institution 1</option>
+                          </select>
+                        </Col>
+                      </Form.Group>
+                    </Col>
+                    <Col className="pr-1" md="4">
+                      <Form.Group>
+                        <label>Street Name</label>
+                        <Form.Control
+                          placeholder="Eligibility Response Identifier"
+                          type="text"
+                        ></Form.Control>
+                      </Form.Group>
+                    </Col>
+                    <Col className="pr-1" md="4">
+                      <Form.Group>
+                        <label>City</label>
+                        <Form.Control
+                          placeholder="Eligibility Response Identifier"
+                          type="text"
+                        ></Form.Control>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="pr-1" md="4">
+                      <Form.Group>
+                        <label>State/ Provoince</label>
+                        <Form.Control
+                          placeholder="State/ Provoince"
+                          type="text"
+                        ></Form.Control>
+                      </Form.Group>
+                    </Col>
+                    <Col className="px-1" md="4">
+                    <Form.Group>
+                        <label class="form-check-label">Country</label>
+                        <Col className="pr-1" md="12">
+                          <select class="custom-select" id="gender2">
+                            <option selected>Choose...</option>
+                            <option value="1">Institution</option>
+                            <option value="2">Institution 1</option>
+                          </select>
+                        </Col>
+                      </Form.Group>
+                    </Col>
+                    <Col className="px-1" md="4">
+                      <Form.Group>
+                        <label>Date</label>
+                        <Form.Control
+                          placeholder="Date"
+                          type="date"
+                        ></Form.Control>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                 
+                  
+                </Form>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
