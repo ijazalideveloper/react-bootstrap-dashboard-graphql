028e10cbbd18dad33b5d648b4e30856dd33031af
ijazali <ijazalideveloper@gmail.com>
2023-03-30 22:48:00 +0500

    beneficary form changes

--- src/components/Beneficary/Accident.js
+++ src/components/Beneficary/Accident.js
@@ -3,92 +3,103 @@ import React, { useState } from "react";
 // react-bootstrap components
 import { Button, Card, Form, Row, Col } from "react-bootstrap";
 function Accident() {
-    return (
+  const [createNewBeneficaryField, setCreateNewBeneficaryField] =
+    useState(false);
+
+  const onClickInsurancePlanFieldAdd = () => {
+    setCreateNewBeneficaryField(!createNewBeneficaryField);
+  };
+  return (
+    <Row>
+      <Col md="12">
         <Card>
-            <Card.Header>
-                <Card.Title as="h4">
-                    <div className="custom-control custom-switch d-inline-flex justify-content-end align-items-center">
-                        <input
-                            type="checkbox"
-                            className="custom-control-input"
-                            id="accident"
-                        />
-                        <label className="custom-control-label mb-0" for="accident">
-                            Accident
-                        </label>
-                    </div>
-                </Card.Title>
-            </Card.Header>
+          <Card.Header>
+            <Card.Title as="h4">
+              <div className="custom-control custom-switch d-inline-flex justify-content-end align-items-center">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="accident"
+                  onChange={onClickInsurancePlanFieldAdd}
+                />
+                <label className="custom-control-label mb-3" for="accident">
+                  Accident
+                </label>
+              </div>
+            </Card.Title>
+          </Card.Header>
+          {createNewBeneficaryField && (
             <Card.Body>
-                <Form>
-                    <Row>
-                        <Col className="px-1" md="4">
-                            <Form.Group>
-                                <label class="form-check-label">Accident Type</label>
-                                <Col className="pr-1" md="12">
-                                    <select class="custom-select" id="gender2">
-                                        <option selected>Choose...</option>
-                                        <option value="1">Institution</option>
-                                        <option value="2">Institution 1</option>
-                                    </select>
-                                </Col>
-                            </Form.Group>
-                        </Col>
-                        <Col className="pr-1" md="4">
-                            <Form.Group>
-                                <label>Street Name</label>
-                                <Form.Control
-                                    placeholder="Eligibility Response Identifier"
-                                    type="text"
-                                ></Form.Control>
-                            </Form.Group>
-                        </Col>
-                        <Col className="pr-1" md="4">
-                            <Form.Group>
-                                <label>City</label>
-                                <Form.Control
-                                    placeholder="Eligibility Response Identifier"
-                                    type="text"
-                                ></Form.Control>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className="pr-1" md="4">
-                            <Form.Group>
-                                <label>State/ Provoince</label>
-                                <Form.Control
-                                    placeholder="State/ Provoince"
-                                    type="text"
-                                ></Form.Control>
-                            </Form.Group>
-                        </Col>
-                        <Col className="px-1" md="4">
-                            <Form.Group>
-                                <label class="form-check-label">Country</label>
-                                <Col className="pr-1" md="12">
-                                    <select class="custom-select" id="gender2">
-                                        <option selected>Choose...</option>
-                                        <option value="1">Institution</option>
-                                        <option value="2">Institution 1</option>
-                                    </select>
-                                </Col>
-                            </Form.Group>
-                        </Col>
-                        <Col className="px-1" md="4">
-                            <Form.Group>
-                                <label>Date</label>
-                                <Form.Control
-                                    placeholder="Date"
-                                    type="date"
-                                ></Form.Control>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                </Form>
+              <Form>
+                <Row>
+                  <Col className="" md="4">
+                    <Form.Group>
+                      <label class="form-check-label">Accident Type</label>
+                      <select class="custom-select" id="gender2">
+                        <option selected>Choose...</option>
+                        <option value="1">Institution</option>
+                        <option value="2">Institution 1</option>
+                      </select>
+                    </Form.Group>
+                  </Col>
+                  <Col className="" md="4">
+                    <Form.Group>
+                      <label>Street Name</label>
+                      <Form.Control
+                        placeholder="Eligibility Response Identifier"
+                        type="text"
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
+                  <Col className="" md="4">
+                    <Form.Group>
+                      <label>City</label>
+                      <Form.Control
+                        placeholder="Eligibility Response Identifier"
+                        type="text"
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="" md="4">
+                    <Form.Group>
+                      <label>State/ Provoince</label>
+                      <Form.Control
+                        placeholder="State/ Provoince"
+                        type="text"
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
+                  <Col className="" md="4">
+                    <Form.Group>
+                      <label class="form-check-label">Country</label>
+                      <Col className="" md="12">
+                        <select class="custom-select" id="gender2">
+                          <option selected>Choose...</option>
+                          <option value="1">Institution</option>
+                          <option value="2">Institution 1</option>
+                        </select>
+                      </Col>
+                    </Form.Group>
+                  </Col>
+                  <Col className="" md="4">
+                    <Form.Group>
+                      <label>Date</label>
+                      <Form.Control
+                        placeholder="Date"
+                        type="date"
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Form>
             </Card.Body>
+          )}
         </Card>
-    );
+      </Col>
+    </Row>
+  );
 }
 
 export default Accident;
---
+++ src/components/Beneficary/AddItemForm.js
@@ -0,0 +1,245 @@
+import React, { useState } from "react";
+// react-bootstrap components
+import { Button, Card, Form, Row, Col } from "react-bootstrap";
+
+function AddItemForm() {
+  return (
+    <Row>
+      <Col md="12">
+        <Card>
+          <Card.Header>
+            <Card.Title as="h4">Add Item</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Form>
+              <Row>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Type</label>
+
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">
+                      Standard Code-Description
+                    </label>
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="pr-1" md="3">
+                  <Form.Group>
+                    <label>Non Standard Code</label>
+                    <Form.Control
+                      placeholder="Enter Non Standard Code"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="pr-1" md="3">
+                  <Form.Group>
+                    <label>Non Standard Description</label>
+                    <Form.Control
+                      placeholder="Non Standard Description"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Body Site</label>
+
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Sub Site</label>
+
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Service Start Date</label>
+                    <Form.Control placeholder="Date" type="date"></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Service End Date</label>
+                    <Form.Control placeholder="Date" type="date"></Form.Control>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Supporting Info</label>
+
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Diagnosis</label>
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Care Team</label>
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label class="form-check-label">Is Package?</label>
+                    <select class="custom-select" id="gender2">
+                      <option selected>Choose...</option>
+                      <option value="1">Institution</option>
+                      <option value="2">Institution 1</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Quantity</label>
+                    <Form.Control
+                      placeholder="Enter Quantity"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="2">
+                  <Form.Group>
+                    <label>Unit Price</label>
+                    <Form.Control
+                      placeholder="Enter Unit Price"
+                      type="number"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="1"></Col>
+                <Col className="" md="1">
+                  <Form.Group>
+                    <label>Tax Amount</label>
+                    <Form.Control
+                      placeholder="Enter Tax Amount"
+                      type="number"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="2">
+                  <Form.Group>
+                    <label>Factor</label>
+                    <Form.Control
+                      placeholder="Enter Factor"
+                      type="number"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Net</label>
+                    <Form.Control
+                      placeholder="Enter Net"
+                      type="number"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Discount Amount</label>
+                    <Form.Control
+                      placeholder="Enter Discount Amount"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="2">
+                  <Form.Group>
+                    <label>Patient Share (%)</label>
+                    <Form.Control
+                      placeholder="Enter Patient Share"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col
+                  className="d-flex justify-content-center align-items-center"
+                  md="1"
+                >
+                  <svg
+                    width="24"
+                    height="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                  >
+                    <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+                  </svg>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Patient Share Amount</label>
+                    <Form.Control
+                      placeholder="Enter Patient Share Amount"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col className="" md="3">
+                  <Form.Group>
+                    <label>Payer Share</label>
+                    <Form.Control
+                      placeholder="Enter Payer Share"
+                      type="text"
+                    ></Form.Control>
+                  </Form.Group>
+                </Col>
+              </Row>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+}
+
+export default AddItemForm;
